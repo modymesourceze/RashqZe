e290d2c10ef628175d7fb3227bdc976ace85f093
modymesourceze <148470115+modymesourceze@users.noreply.github.com>
2023-12-27 17:12:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
-from config import Config
 import pyrogram , pyromod
+from config import Config 
 from pyromod import listen
+
 from pyrogram import Client, filters, enums
 from kvsqlite.sync import Client as dt
 p = dict(root='plugins')
-tok = Config.TG_BOT_TOKEN ## توكنك 
-id = Config.APP_ID ## ايديك حبي
+tok = Config.TG_BOT_TOKEN 
+id = 6581896306 ## ايديك
 db = dt("data.sqlite", 'fuck')
+if not db.get("checker"):
+  db.set('checker', None)
 if not db.get("admin_list"):
-  db.set('admin_list', [id, 6581896306]) # اضف المزيد من الادمنية في هذه الليست حبي
+  db.set('admin_list', [id, 6581896306])
 if not db.get('ban_list'):
   db.set('ban_list', [])
+if not db.get('sessions'):
+  db.set('sessions', [])
 if not db.get('force'):
-  db.set('force', ['Source_Ze'])
+  db.set('force', ['source_ze'])
 x = Client(name='loclhosst', api_id=Config.APP_ID, api_hash=Config.API_HASH, bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
 
 x.run()
